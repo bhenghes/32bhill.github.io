0dd19b47a9927bcee053a6359999d8e1c3668360
bhenghes <bhenghes@gmail.com>
2025-08-01 13:20:22 +0100

    added floorplan

--- data.js
+++ data.js
@@ -1,7 +1,131 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-front-room",
+      "id": "0-floorplan",
+      "name": "floorplan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.0271044358418564,
+        "pitch": -0.006145438981675966,
+        "fov": 1.4083983627516938
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3548802238273936,
+          "pitch": 0.3569336263970708,
+          "rotation": 0,
+          "target": "2-hall-1"
+        },
+        {
+          "yaw": -0.1489015111687504,
+          "pitch": 0.44284597102598156,
+          "rotation": 0,
+          "target": "1-front-room"
+        },
+        {
+          "yaw": -0.18363185729886666,
+          "pitch": 0.04929767885070113,
+          "rotation": 0,
+          "target": "5-kitchen"
+        },
+        {
+          "yaw": -0.09911124115891745,
+          "pitch": 0.21972190622062726,
+          "rotation": 0,
+          "target": "4-kitchen-counter"
+        },
+        {
+          "yaw": -0.2810768255840692,
+          "pitch": 0.2551871062629374,
+          "rotation": 0,
+          "target": "7-cellar-1"
+        },
+        {
+          "yaw": -0.2018096940796692,
+          "pitch": -0.15013493494109298,
+          "rotation": 0,
+          "target": "6-downstairs-bathroom"
+        },
+        {
+          "yaw": -0.35317448952754305,
+          "pitch": 0.004007713956104553,
+          "rotation": 0,
+          "target": "3-hall-2"
+        },
+        {
+          "yaw": -0.25341615388355265,
+          "pitch": -0.6787043863203017,
+          "rotation": 0,
+          "target": "15-back-garden"
+        },
+        {
+          "yaw": 0.20107813786068007,
+          "pitch": -0.5060939671582254,
+          "rotation": 0,
+          "target": "14-back-bedroom"
+        },
+        {
+          "yaw": -0.03371979775999279,
+          "pitch": -0.5336194331833006,
+          "rotation": 0,
+          "target": "17-alley-garden"
+        },
+        {
+          "yaw": 0.26635295229805145,
+          "pitch": -0.3102333564362212,
+          "rotation": 0,
+          "target": "18-small-bedroom"
+        },
+        {
+          "yaw": 0.28120569106250315,
+          "pitch": -0.10513484991940558,
+          "rotation": 0,
+          "target": "10-upstairs-bathroom"
+        },
+        {
+          "yaw": 0.11945848925965663,
+          "pitch": 0.042497316607397195,
+          "rotation": 0,
+          "target": "13-upstairs-hall"
+        },
+        {
+          "yaw": 0.12810943537503228,
+          "pitch": 0.23673468867538006,
+          "rotation": 0,
+          "target": "11-loft-1"
+        },
+        {
+          "yaw": 0.19170771362823125,
+          "pitch": 0.40829775050034556,
+          "rotation": 0,
+          "target": "19-primary-bedroom-1"
+        },
+        {
+          "yaw": 0.3156308325579644,
+          "pitch": 0.5567950226680676,
+          "rotation": 0,
+          "target": "20-primary-bedroom-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-front-room",
       "name": "Front Room",
       "levels": [
         {
@@ -37,13 +161,13 @@ var APP_DATA = {
           "yaw": 2.2262767200132263,
           "pitch": 0.2710086276864807,
           "rotation": 0,
-          "target": "1-hall-1"
+          "target": "2-hall-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-hall-1",
+      "id": "2-hall-1",
       "name": "Hall 1",
       "levels": [
         {
@@ -79,25 +203,25 @@ var APP_DATA = {
           "yaw": -1.5502032667253118,
           "pitch": 0.27741226910993966,
           "rotation": 0,
-          "target": "0-front-room"
+          "target": "1-front-room"
         },
         {
           "yaw": 3.011338046137749,
           "pitch": 0.08444808956201655,
           "rotation": 0,
-          "target": "2-hall-2"
+          "target": "3-hall-2"
         },
         {
           "yaw": -2.796502809730786,
           "pitch": -0.002018095657209784,
           "rotation": 0,
-          "target": "8-stairs"
+          "target": "9-stairs"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-hall-2",
+      "id": "3-hall-2",
       "name": "Hall 2",
       "levels": [
         {
@@ -133,37 +257,37 @@ var APP_DATA = {
           "yaw": 0.06134305290375508,
           "pitch": -0.0029495774157233257,
           "rotation": 0,
-          "target": "4-kitchen"
+          "target": "5-kitchen"
         },
         {
           "yaw": 0.5788760167684259,
           "pitch": 0.35124639471619723,
           "rotation": 0,
-          "target": "6-cellar-1"
+          "target": "7-cellar-1"
         },
         {
           "yaw": -1.2250568776801423,
           "pitch": 0.022572886803724046,
           "rotation": 0,
-          "target": "5-downstairs-bathroom"
+          "target": "6-downstairs-bathroom"
         },
         {
           "yaw": 1.7589551235698089,
           "pitch": 0.07017351721412979,
           "rotation": 0,
-          "target": "1-hall-1"
+          "target": "2-hall-1"
         },
         {
           "yaw": 1.4156214867537926,
           "pitch": -0.5559208515511251,
           "rotation": 0,
-          "target": "8-stairs"
+          "target": "9-stairs"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-kitchen-counter",
+      "id": "4-kitchen-counter",
       "name": "Kitchen counter",
       "levels": [
         {
@@ -199,13 +323,13 @@ var APP_DATA = {
           "yaw": 3.134825691602485,
           "pitch": 0.16220842978988514,
           "rotation": 0,
-          "target": "4-kitchen"
+          "target": "5-kitchen"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-kitchen",
+      "id": "5-kitchen",
       "name": "Kitchen",
       "levels": [
         {
@@ -241,25 +365,25 @@ var APP_DATA = {
           "yaw": -0.15310242102123794,
           "pitch": 0.04975643192732093,
           "rotation": 0,
-          "target": "2-hall-2"
+          "target": "3-hall-2"
         },
         {
           "yaw": -2.255154778716303,
           "pitch": 0.03859380934850698,
           "rotation": 0,
-          "target": "3-kitchen-counter"
+          "target": "4-kitchen-counter"
         },
         {
           "yaw": 1.7695625547105394,
           "pitch": -0.13270469305404475,
           "rotation": 0,
-          "target": "16-alley-garden"
+          "target": "17-alley-garden"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-downstairs-bathroom",
+      "id": "6-downstairs-bathroom",
       "name": "Downstairs Bathroom",
       "levels": [
         {
@@ -295,13 +419,13 @@ var APP_DATA = {
           "yaw": 2.943033117908355,
           "pitch": 0.19280867298890847,
           "rotation": 0,
-          "target": "2-hall-2"
+          "target": "3-hall-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-cellar-1",
+      "id": "7-cellar-1",
       "name": "Cellar 1",
       "levels": [
         {
@@ -337,19 +461,19 @@ var APP_DATA = {
           "yaw": 1.3578341179330131,
           "pitch": 0.885642585768819,
           "rotation": 0,
-          "target": "7-cellar-2"
+          "target": "8-cellar-2"
         },
         {
           "yaw": -3.057354453007175,
           "pitch": -0.40868476155945643,
           "rotation": 0,
-          "target": "2-hall-2"
+          "target": "3-hall-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-cellar-2",
+      "id": "8-cellar-2",
       "name": "Cellar 2",
       "levels": [
         {
@@ -385,19 +509,19 @@ var APP_DATA = {
           "yaw": 1.6145936100097389,
           "pitch": 0.21657298565258287,
           "rotation": 0,
-          "target": "6-cellar-1"
+          "target": "7-cellar-1"
         },
         {
           "yaw": 0.747965888212974,
           "pitch": -0.28129872416011814,
           "rotation": 0,
-          "target": "2-hall-2"
+          "target": "3-hall-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-stairs",
+      "id": "9-stairs",
       "name": "Stairs",
       "levels": [
         {
@@ -433,25 +557,25 @@ var APP_DATA = {
           "yaw": -3.0543049839403302,
           "pitch": 0.40557698342598414,
           "rotation": 0,
-          "target": "1-hall-1"
+          "target": "2-hall-1"
         },
         {
           "yaw": -1.316652826610202,
           "pitch": 1.0159639470601896,
           "rotation": 0,
-          "target": "2-hall-2"
+          "target": "3-hall-2"
         },
         {
           "yaw": -0.7088866169133485,
           "pitch": -0.6374441131980575,
           "rotation": 0,
-          "target": "12-upstairs-hall"
+          "target": "13-upstairs-hall"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-upstairs-bathroom",
+      "id": "10-upstairs-bathroom",
       "name": "Upstairs Bathroom",
       "levels": [
         {
@@ -487,13 +611,13 @@ var APP_DATA = {
           "yaw": 1.826566411658991,
           "pitch": 0.45143490388247365,
           "rotation": 0,
-          "target": "12-upstairs-hall"
+          "target": "13-upstairs-hall"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10-loft-1",
+      "id": "11-loft-1",
       "name": "Loft 1",
       "levels": [
         {
@@ -529,19 +653,19 @@ var APP_DATA = {
           "yaw": 0.47590638107649674,
           "pitch": 1.196030515979329,
           "rotation": 0,
-          "target": "12-upstairs-hall"
+          "target": "13-upstairs-hall"
         },
         {
           "yaw": 1.3656431122446158,
           "pitch": 0.382183180707413,
           "rotation": 0,
-          "target": "11-loft-2"
+          "target": "12-loft-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "11-loft-2",
+      "id": "12-loft-2",
       "name": "Loft 2",
       "levels": [
         {
@@ -577,19 +701,19 @@ var APP_DATA = {
           "yaw": -2.746902431557775,
           "pitch": 1.0847446463437933,
           "rotation": 0,
-          "target": "12-upstairs-hall"
+          "target": "13-upstairs-hall"
         },
         {
           "yaw": 2.0595587508459214,
           "pitch": 0.2363346944236806,
           "rotation": 0,
-          "target": "10-loft-1"
+          "target": "11-loft-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "12-upstairs-hall",
+      "id": "13-upstairs-hall",
       "name": "Upstairs Hall",
       "levels": [
         {
@@ -625,43 +749,43 @@ var APP_DATA = {
           "yaw": -0.3416299345360727,
           "pitch": 0.06803012298825806,
           "rotation": 0,
-          "target": "13-back-bedroom"
+          "target": "14-back-bedroom"
         },
         {
           "yaw": -0.2232424850551844,
           "pitch": 0.08957847020577425,
           "rotation": 0,
-          "target": "17-small-bedroom"
+          "target": "18-small-bedroom"
         },
         {
           "yaw": -0.06662295698931686,
           "pitch": 0.20029361812876267,
           "rotation": 0,
-          "target": "9-upstairs-bathroom"
+          "target": "10-upstairs-bathroom"
         },
         {
           "yaw": 1.472122221076913,
           "pitch": 0.6129602591190988,
           "rotation": 0,
-          "target": "8-stairs"
+          "target": "9-stairs"
         },
         {
           "yaw": 2.715724375985488,
           "pitch": 0.11465547472538162,
           "rotation": 0,
-          "target": "18-primary-bedroom-1"
+          "target": "19-primary-bedroom-1"
         },
         {
           "yaw": 2.627703553694449,
           "pitch": -0.5120849431636145,
           "rotation": 0,
-          "target": "10-loft-1"
+          "target": "11-loft-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "13-back-bedroom",
+      "id": "14-back-bedroom",
       "name": "Back bedroom",
       "levels": [
         {
@@ -697,19 +821,19 @@ var APP_DATA = {
           "yaw": 0,
           "pitch": 0,
           "rotation": 0,
-          "target": "15-garden"
+          "target": "16-garden"
         },
         {
           "yaw": -3.0469665726658803,
           "pitch": 0.14400053015057246,
           "rotation": 0,
-          "target": "12-upstairs-hall"
+          "target": "13-upstairs-hall"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "14-back-garden",
+      "id": "15-back-garden",
       "name": "Back Garden",
       "levels": [
         {
@@ -745,13 +869,13 @@ var APP_DATA = {
           "yaw": -2.931439488079514,
           "pitch": 0.13408714797555454,
           "rotation": 0,
-          "target": "15-garden"
+          "target": "16-garden"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "15-garden",
+      "id": "16-garden",
       "name": "Garden",
       "levels": [
         {
@@ -787,25 +911,25 @@ var APP_DATA = {
           "yaw": 0.9493650465592047,
           "pitch": 0.16478470241748155,
           "rotation": 0,
-          "target": "14-back-garden"
+          "target": "15-back-garden"
         },
         {
           "yaw": -2.1249337609789904,
           "pitch": -0.5626736823377865,
           "rotation": 0,
-          "target": "13-back-bedroom"
+          "target": "14-back-bedroom"
         },
         {
           "yaw": -2.499704550271801,
           "pitch": 0.08105961551170537,
           "rotation": 0,
-          "target": "16-alley-garden"
+          "target": "17-alley-garden"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "16-alley-garden",
+      "id": "17-alley-garden",
       "name": "Alley Garden",
       "levels": [
         {
@@ -841,13 +965,13 @@ var APP_DATA = {
           "yaw": 1.964964961969324,
           "pitch": 0.3177723812110642,
           "rotation": 0,
-          "target": "15-garden"
+          "target": "16-garden"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "17-small-bedroom",
+      "id": "18-small-bedroom",
       "name": "Small bedroom",
       "levels": [
         {
@@ -879,13 +1003,13 @@ var APP_DATA = {
           "yaw": -1.3965134648878,
           "pitch": 0.3182633745306642,
           "rotation": 0,
-          "target": "12-upstairs-hall"
+          "target": "13-upstairs-hall"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "18-primary-bedroom-1",
+      "id": "19-primary-bedroom-1",
       "name": "Primary bedroom 1",
       "levels": [
         {
@@ -917,19 +1041,19 @@ var APP_DATA = {
           "yaw": 3.1302884705871294,
           "pitch": 0.07894389690756753,
           "rotation": 0,
-          "target": "12-upstairs-hall"
+          "target": "13-upstairs-hall"
         },
         {
           "yaw": -0.594058362325713,
           "pitch": 0.18191830811988652,
           "rotation": 0,
-          "target": "19-primary-bedroom-2"
+          "target": "20-primary-bedroom-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "19-primary-bedroom-2",
+      "id": "20-primary-bedroom-2",
       "name": "Primary bedroom 2",
       "levels": [
         {
@@ -961,7 +1085,7 @@ var APP_DATA = {
           "yaw": 2.6173170400534307,
           "pitch": 0.24064839913483738,
           "rotation": 0,
-          "target": "18-primary-bedroom-1"
+          "target": "19-primary-bedroom-1"
         }
       ],
       "infoHotspots": []
